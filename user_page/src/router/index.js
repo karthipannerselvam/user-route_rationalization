986848102ca79bd9d506d63da12cc385a2f1453a
karthipannerselvam <118588657+karthipannerselvam@users.noreply.github.com>
2024-12-12 11:47:45 +0530

    added gps location

--- user_page/src/router/index.js
+++ user_page/src/router/index.js
@@ -3,6 +3,7 @@ import RouteForm from '../views/RouteForm.vue'
 import dashboard from '../views/dashboard.vue'
 import aboutus from '../views/aboutus.vue'
 import Route from '../views/routes.vue'
+import NearbyBuses from '../views/NearbyBuses.vue'
 
 const router = createRouter({
     history:createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
             name:'aboutus',
             component:aboutus,
           },
+          {
+            path:"/near",
+            name:NearbyBuses,
+            component:NearbyBuses
+          }
     ]
 })
 
